fbd62b48303ac7aff98a3e392bf0b4711ea39035
Lukáš Rác <rac@uxf.cz>
2024-06-08 12:48:52 +0200

    added lost pixel workflow

---
+++ lostpixel.config.ts
@@ -0,0 +1,10 @@
+import { CustomProjectConfig } from "lost-pixel";
+
+export const config: CustomProjectConfig = {
+    storybookShots: {
+        storybookUrl: "./storybook-static",
+    },
+
+    lostPixelProjectId: "clx5zl20n54csh7topebm5l90",
+    apiKey: process.env.LOST_PIXEL_API_KEY,
+};
